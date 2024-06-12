826cd505f4c8774d393b4196cd5c178ec8037d41
yusuffadil <yusuffadil417@gmail.com>
2024-06-13 00:06:37 +0700

    commit

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import { Karla } from 'next/font/google'
+import './globals.css'
+import { ThemeProvider } from '@/components/theme-provider';
+import { Metadata } from 'next';
+
+const karla = Karla({
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+  subsets: ['latin'],
+  variable: "--font-karla"
+})
+export const metadata: Metadata = {
+  title: "admin dashboard",
+  description: "NextJs admin dashboard"
+};
+
+export default function RootLayout({ children, }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={karla.className + ' h-screen overflow-hidden'}>
+        <ThemeProvider
+          themes={['dark', 'custom', 'light']}
+          attribute="class"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
